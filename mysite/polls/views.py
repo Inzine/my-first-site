31103def01f260dd9593bcfab4ffcd0619792190
inzine <inzine@gmail.com>
2018-07-01 01:17:57 -0700

    Changed the HTML for the site.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
+#from django.shortcuts import HttpResponse
 
-def index(request):
-	return HttpResponse("FEEDs ZONE")
+#def index(request):
+	#return HttpResponse("FEEDs ZONE")
+
+def post_list(request):
+	return render(request, 'polls/post_list.html', {})
